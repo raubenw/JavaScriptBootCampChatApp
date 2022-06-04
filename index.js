25ce7dd78081d3c7fdc29121a9f95afbd428cfc4
Werner Raubenheimer <Wernerraubenheimer@cloudbadger.com>
2022-06-04 20:41:06 +0200

    Added scoket events to chat rooms

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ app.get('/python', (req, res) => {
     res.sendFile(__dirname + '/public/python.html');
 });
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+const tech = io.of('/tech');
+
+tech.on('connection', (socket) => {
+
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New User Joinded ${data.room} room!`);
+    });
+    
+    socket.on('message', (data) => {
+        tech.in(data.room).emit('message', data.msg);
+    });
+
+    io.on('discconect', () => {
+        console.log('User Disconnected')
+        tech.emit('message', 'Users Disconnected');
+    });
+
 });
